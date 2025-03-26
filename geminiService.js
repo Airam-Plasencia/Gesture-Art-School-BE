55374c2523d178ae54dde8c6b629e2689de6f75a
Airam Plasencia <airaplas@gmail.com>
2025-03-26 11:42:43 +0000

    Update prompt GeminiService

--- geminiService.js
+++ geminiService.js
@@ -9,22 +9,35 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 async function generateCourseRecommendations(text) {
     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
-    const prompt = `genera una lista detallada de 3 líneas como máximo de la información escrita ${text}`;
+    const prompt = `genera una lista detallada de 3 líneas como máximo de la información escrita ${text}, además formatea la respuesta con salto de línea (\\n) para separar párrafos y listas, usa (-) para las listas en lugar de asterisco (*)`;
 
     try {
         const result = await model.generateContent(prompt);
         const response = await result.response;
-        const text = response.text();
+        let responseText = response.text();
 
-        // Formatear la respuesta como un array de strings
-        const recommendationsArray = text.split('\n').filter(line => line.trim() !== '');
-        return recommendationsArray;
+        // Dividir el texto en párrafos
+        let paragraphs = responseText.split('\n\n').filter(paragraph => paragraph.trim() !== '');
+
+        // Eliminar los símbolos ** y ordenar alfabéticamente
+        const orderedParagraphs = paragraphs.map(paragraph => {
+            let trimmedParagraph = paragraph.trim();
+            if (trimmedParagraph.startsWith('**')) {
+                return trimmedParagraph.substring(2).trim(); // Eliminar los dos asteriscos
+            } else {
+                return trimmedParagraph;
+            }
+        }).sort();
+
+        // Reconstruir la respuesta con los párrafos ordenados
+        let orderedResponse = orderedParagraphs.join('\n\n');
+
+        return orderedResponse;
 
     } catch (error) {
         console.error("Error al generar recomendaciones:", error);
-        return []; // Devuelve un array vacío en caso de error
+        return ""; // Devuelve un string vacío en caso de error
     }
 }
-
 
 module.exports = { generateCourseRecommendations };
